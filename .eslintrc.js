121a4679b442ec27bf718d053b48150971a34259
Kagami <moe.pascal@gmail.com>
2017-07-12 00:36:55 +0800

    add eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,29 @@
+module.exports = {
+  'env': {
+    'browser': true,
+  },
+  'extends': [
+    "airbnb",
+  ],
+  'parser': 'babel-eslint',
+  'plugins': [
+    'import',
+    'react',
+  ],
+  'rules': {
+    'semi': ['error', 'never'],
+    'react/jsx-filename-extension': 'off',
+    'no-underscore-dangle': ['error', { 'allow': ['__'], 'allowAfterThis': true }],
+    'comma-dangle': ['error', 'always-multiline'],
+    'no-confusing-arrow': 'off',
+    'no-console': ['error', {allow: ['warn', 'error']}],
+  },
+  'settings': {
+    'import/resolver': {
+      'node': {
+        'extensions': ['.js', '.jsx', '.es'],
+        'paths': [__dirname],
+      },
+    },
+  },
+}
