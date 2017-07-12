6ec2b50baf9856612268d7d27d0258d70a4152ab
Kagami <moe.pascal@gmail.com>
2017-07-12 23:50:17 +0800

    add css

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+import 'typeface-roboto/index.css'
+import 'material-design-icons-iconfont-only/iconfont/material-icons.css'
+
 import './index.css'
 import App from './app'
 import registerServiceWorker from './registerServiceWorker'
