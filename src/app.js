6dfd77d2640be6e07374e11c657bcb34614c430f
Kagami <moe.pascal@gmail.com>
2017-07-13 01:16:28 +0800

    add appbar

--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 
-import logo from './logo.svg'
-import styles from './app.css'
+import { AppBar, Toolbar, Typography, Button } from 'material-ui'
+
+import { withStyles, createStyleSheet } from 'material-ui/styles'
+
+import logo from './assets/logo.png'
+
+const styles = createStyleSheet('MainApp', {
+  header: {
+    flex: 1,
+  },
+})
 
 class App extends Component {
   render() {
+    const { classes } = this.props
     return (
-      <div className={styles.App}>
-      </div>
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography type="title" color="inherit" className={classes.header}>
+            <img src={logo} alt="Atelier Yuubari" height={48} />
+          </Typography>
+          <Button>About</Button>
+        </Toolbar>
+      </AppBar>
     )
   }
 }
 
-export default App
+export default withStyles(styles)(App)
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import { MuiThemeProvider, createMuiTheme, createPalette, withStyles, createStyleSheet } from 'material-ui/styles'
+import lightGreen from 'material-ui/colors/lightGreen'
+import amber from 'material-ui/colors/amber'
+
 import 'typeface-roboto/index.css'
 import 'material-design-icons-iconfont-only/iconfont/material-icons.css'
 
@@ -8,5 +12,12 @@ import './index.css'
 import App from './app'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const theme = createMuiTheme({
+  palette: createPalette({
+    primary: lightGreen,
+    accent: amber,
+  }),
+})
+
+ReactDOM.render(<MuiThemeProvider theme={theme}><App /></MuiThemeProvider>, document.getElementById('root'))
 registerServiceWorker()
